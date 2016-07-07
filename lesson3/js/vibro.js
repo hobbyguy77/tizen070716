bbf9919c47b0ab96b3dcacb1c86404a062aeb16b
hobbyguy77 <markmak77@gmail.com>
2016-07-07 15:47:58 +0300

    Bluetooth and vibration

---
+++ lesson3/js/vibro.js
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+
+function singleVib() {
+	navigator.vibrate(1000);
+}
+
+function multiVib() {
+	var command = "[2000";
+	for (var i = 2000; i > 200; i = i - 200)
+		command = command + "," + i;
+	command = command + "]";
+	var array = JSON.parse(command);
+	navigator.vibrate(array);
+}
+
+function stopVib() {
+	navigator.vibrate(0);
+}
+
+var adapter = null;
+var CHECK_INTERVAL = 1000;
+
+(function checkAdapter() {
+	try {
+		if (tizen.bluetooth == undefined) {
+			alert("No bluetooth adapter found.");
+		} else {
+			adapter = tizen.bluetooth.getDefaultAdapter();
+			window.setInterval(checkSliderBT, CHECK_INTERVAL);
+		}
+
+	} catch (e) {
+		alert(e);
+	}
+}());
+
+function checkSliderBT() {
+	if (adapter.powered) {
+		$("#btSlider").val("on");
+	} else {
+		$("#btSlider").val("off");
+	}
+	$("#btSlider").slider("refresh");
+}
+
+function switchBT() {
+	if ($("#btSlider").val() == "on")
+		BTpowerOn();
+	else
+		BTpowerOff();
+}
+
+function BTpowerOff() {
+	adapter.setPowered(false, null, null);
+}
+
+function BTpowerOn() {
+	adapter.setPowered(true, null, function(e){alert(e)});
+}
